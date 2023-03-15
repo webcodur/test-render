26790f8b04802397a0c646e3d9d8edf3c92d9426
윤시준 <webcodur@gmail.com>
2023-03-15 15:32:24 +0900

    directory changed

--- front/component/Header.tsx
+++ front/component/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
     <>
       <h3>header</h3>
       <Link href="/">home</Link><br />
-      <Link href="/csr">csr</Link><br />
-      <Link href="/ssr">ssr</Link><br />
-      <Link href="/isr">isr</Link><br />
-      <Link href="/ssg">ssg</Link><br />
+      <Link href="/render/csr">csr</Link><br />
+      <Link href="/render/ssr">ssr</Link><br />
+      <Link href="/render/isr">isr</Link><br />
+      <Link href="/render/ssg">ssg</Link><br />
       <br />
     </>
   );
